2b412ecc3ed14bbb013a5604f3fb0af5519dbba5
ironshadowz <chenwl911@gmail.com>
2023-04-18 14:47:12 +0800

    workshop

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,5 +1,10 @@
 package sg.edu.nus.iss;
 
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,65 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Welcome to your shopping cart");
+        Console con = System.console();
+       
+        List<String> Cart = new ArrayList<String>();
+        
+        String input = "";
+        while (!input.equals("quit"))
+        {
+            input = con.readLine("Type 'help' to show list of commands>>");
+            if(input.equals("help"))
+            {
+                System.out.println("'list' to show a list of items in the shopping cart");
+                System.out.println("'add' <item name>, ...");
+                System.out.println("'delete' <item number>");
+                System.out.println("'quit' to terminate the program");
+            }
+
+            if(input.startsWith("add"))
+            {
+                input=input.replace(',', ' ');
+                Scanner scan = new Scanner(input.substring(4));
+                String content = "";
+                while(scan.hasNext()){
+
+                    content=scan.next();
+
+                    if(Cart.contains(content)){
+                        System.out.println(content+" already in cart.");
+                    } else { 
+                        Cart.add(content);
+                    }
+                }
+            }
+            if(input.equals("list"))
+            {
+                int i =0;
+                for(String item: Cart)
+                {
+                    i++;
+                    System.out.println(i+". "+item);
+                }
+            }
+            if(input.equals("delete"))
+            {
+                Scanner scan = new Scanner(input.substring(6));
+                String content="";
+                while(scan.hasNext())
+                {
+                    content=scan.next();
+                    int listIndex = Integer.parseInt(content);
+                    if(listIndex < Cart.size())
+                    {
+                        Cart.remove(listIndex);
+                    } else {
+                        System.err.println("incorrect item index");
+                    }
+                }
+            }
+        }
+        System.out.println("Good Bye");
     }
 }
